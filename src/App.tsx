ef7592fa5b298924757df0e8d6bcc18822ae077e
swapnilRahangdale <rahangdaleswapnil7030@gmail.com>
2023-06-01 17:15:54 +0530

    Main:CompanyName

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,49 @@ import React, { useEffect ,useState  } from 'react';
 
 import './App.css';
 
-function App() {
 
-  const [title , setTitle]   = useState('No Change');
 
-    const userNameApi = () =>{
+function App() { 
 
-         return new Promise<any>((resolve, reject) =>{
+  const [title , setTitle] = useState('Company Name is ')
 
-          return setTimeout(() =>{
-            resolve({name: 'Swapnil'});
-          },1000);
+  const userCompanyName =() =>{
+   
+    return new Promise<any>((resolve,reject) =>{
 
-         })
-    };
+      return setTimeout(() =>{
+        resolve({Company: 'TCS'});
+      },1000);
 
-     
+    })
 
-      const getUserName = async() => {
-       
+  }
+ 
+  const getUserCompanyName = async() =>{
 
-        console.log('test before')
+    console.log('test Before');
 
-        const userData =await userNameApi().catch((err) => {console.log('err',err)});
+    const userData = await userCompanyName().catch((err) =>{console.log('err',err)})
 
-        console.log('userData',userData)
-        const {name} = userData;
-        setTitle(name)
-        
-        console.log('test After res');
-        
-        
-        
-      }
-
+    console.log('userData',userData);
+    const{Company} = userData;
+    setTitle(Company)
     
-     useEffect(() =>{
+    console.log('test After');
+    
+  }
 
-      getUserName()
-         
+  useEffect(()=>{
 
-     }, [] )
+    getUserCompanyName()
+
+  },[])
+  
 
   return (
+
    <div>
-        Name:- {title}
+        Company Name:- {title}
    </div>
   );
 }
